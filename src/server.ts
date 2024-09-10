286f04ec21fa15c11bc7f5297a278ffa474040ee
nelio <neliobino@gmail.com>
2024-09-10 16:43:46 +0200

    primeira fase, ensaios

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,36 @@
 import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
+import fastifyCors from "@fastify/cors";
+import { z } from "zod"
 const app = fastify()
 const db = new PrismaClient()
+
+
+app.register(fastifyCors, {
+    methods:["GET"],
+}
+)
 
 app.get("/users", async (request, reply) => {
     const users = await db.user.findMany()
     return { users }
 })
 
+app.post("/users", async (request, reply) => {
+
+    const createUserSchema = z.object({
+        email: z.string().email(),
+        name: z.string()
+    })
+    const { email, name } = createUserSchema.parse(request.body)
+    await db.user.create({
+        data: {
+            email,
+            name
+        }
+    })
+    reply.status(201).send()
+})
 
 
 app.listen(
